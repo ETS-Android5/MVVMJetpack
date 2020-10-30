b26ffa0a0e818bd5801a8cbe566d13af0806c0bd
zhangt2-DESKTOP <942772119@qq.com>
2020-10-30 13:59:48 +0800

    点名

--- justsy/domain/src/main/java/com/justsafe/domain/api/BaseApi.java
+++ justsy/domain/src/main/java/com/justsafe/domain/api/BaseApi.java
@@ -1,9 +1,9 @@
 package com.justsafe.domain.api;
 
 public interface BaseApi {
-    static final String HOST_TEST = "http://elabtest.e-am.cn/app/server/";
-    static final String HOST = "http://productelab.e-lab.cn/app/server/";
+    static final String HOST_TEST = "http://XXXX/app/server/";
+    static final String HOST = "http://XXXX/app/server/";
 
-    static final String JUST_H5 = "http://meeting.e-lab.cn/watch/";
-    static final String JUST_PROTOCAL = "http://promanager.e-lab.cn/useragreement.html";
+    static final String JUST_H5 = "http://XXXX/watch/";
+    static final String JUST_PROTOCAL = "http://XXXX/useragreement.html";
 }
